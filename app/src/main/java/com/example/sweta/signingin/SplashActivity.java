927b5cbc14a6ebe7ddf2c72e897fcf83d5cbe17b
sw3taprajapati <swetaprajapati806@gmail.com>
2018-03-06 08:03:57 +0545

    splash screen added

---
+++ app/src/main/java/com/example/sweta/signingin/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.example.sweta.signingin;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.preference.PreferenceManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sweta.signingin.MyActivities.MainActivity;
+
+import static android.preference.PreferenceManager.*;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        SharedPreferences sharedPreferences = getDefaultSharedPreferences(this);
+        final Boolean isLoggedIn = sharedPreferences.getBoolean("IsLoggedIn", false);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (isLoggedIn) {
+
+                    Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                    startActivity(intent);
+
+                } else {
+                    Intent intent = new Intent(SplashActivity.this, SigninActivity.class);
+                    startActivity(intent);
+                }
+            }
+        },2000);
+
+    }
+}
